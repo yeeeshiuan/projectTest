7a19d17275ff9026c4e588f2019ff54a3fbce8f3
Joey <u9612130@yuntech.edu.tw>
2019-07-23 18:03:13 +0800

    Add account register router template

--- test12/account/views.py
+++ test12/account/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from account.forms import UserForm
+
+
+def register(request):
+    '''
+    Register a new user
+    '''
+    template = 'account/register.html'
+    if request.method == 'GET':
+        return render(request, template, {'userForm':UserForm()})
+
+    # POST
+    userForm = UserForm(request.POST)
+    if not userForm.is_valid():
+        return render(request, template, {'userForm':userForm})
+
+    userForm.save()
+    return redirect('account:register')
