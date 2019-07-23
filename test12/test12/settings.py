8bac6f67ff593c28975eed15b40d2c311dca004e
Joey <u9612130@yuntech.edu.tw>
2019-07-23 17:16:01 +0800

    default setting and add account app

--- test12/test12/settings.py
+++ test12/test12/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 't(2gzl92ei0mcq8k6b(=8qx$-&&_hzq*d_+_+&@obv&jozo#t6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hant'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Taipei'
 
 USE_I18N = True
 
