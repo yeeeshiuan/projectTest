5b92217e2ebee8db0d9a4461e03fd12e80c937a4
yeeeshiuan <yeeeshiuan.huang@gmail.com>
2019-07-25 19:52:20 +0800

    Update user model and Add UserSession model

--- test12/account/forms.py
+++ test12/account/forms.py
@@ -1,6 +1,6 @@
 from django import forms
-from django.contrib.auth.models import User
 
+from account.models import User
 
 class UserForm(forms.ModelForm):
     username = forms.CharField(label='帳號') 
---
+++ test12/account/models.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.conf import settings
+from django.contrib.sessions.models import Session
+
+
+class User(AbstractUser):
+    pass
+
+class UserSession(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    session = models.OneToOneField(Session, on_delete=models.CASCADE)
